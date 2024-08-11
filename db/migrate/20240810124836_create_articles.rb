7165180538f5ae442658314dc0819cab203820af
Newburu <newburu@gmail.com>
2024-08-11 16:11:36 +0900

    記事を登録できるようにする Fixes #7

---
+++ db/migrate/20240810124836_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration[7.2]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :content
+      t.references :creator, null: false, foreign_key: { to_table: :users, column: :creator_id }
+
+      t.timestamps
+    end
+  end
+end
