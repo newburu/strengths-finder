4658484dc0def192441c77e1d559ff7dec4de55e
Newburu <newburu@gmail.com>
2024-08-15 13:13:11 +0900

    Revert "puma.rbを修正"
    This reverts commit 2a5da382eaa9f0bd777a21b0a74a53a24f772839.

--- config/puma.rb
+++ config/puma.rb
@@ -13,12 +13,8 @@ threads min_threads_count, max_threads_count
 
 # Specifies that the worker count should equal the number of processors in production.
 if ENV["RAILS_ENV"] == "production"
-  worker_count = Integer(
-    ENV.fetch("WEB_CONCURRENCY") do
-      require "concurrent-ruby"
-      Concurrent.physical_processor_count
-    end
-  )
+  require "concurrent-ruby"
+  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
   workers worker_count if worker_count > 1
 end
 
