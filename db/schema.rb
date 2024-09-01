dbd22835868e448c14e8440f3a1a7b902571db8d
にゅ〜ぶる <newburu@gmail.com>
2024-09-01 16:30:53 +0900

    Deviseを使ってログイン機能(GoogleでのSSO)を作る (#14)
    * Deviseを使ってログイン機能(GoogleでのSSO)を作る
    Fixes #12
    
    * GoogleでSSO出来るようにする

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_10_124836) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_18_024057) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -44,6 +44,25 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_10_124836) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "uid"
+    t.string "screen_name"
+    t.string "provider"
+    t.string "password"
+    t.string "access_token"
+    t.string "access_token_secret"
+    t.string "image"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "articles", "users", column: "creator_id"
