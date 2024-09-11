e343152b99826a87e32c28afb4bc45082ba7cf74
Newburu <newburu@gmail.com>
2024-09-11 21:51:02 +0900

    Twitter(X)でログインできるようにする

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,18 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :google_oauth2
 
+  def twitter
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "X") if is_navigational_format?
+    else
+      session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
+  end
+
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
