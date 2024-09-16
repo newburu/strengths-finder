871e01701dd1426355733d2052e6a703c7f0b9dd
Newburu <newburu@gmail.com>
2024-09-16 20:55:53 +0900

    ログイン制御を設定

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
   before_action :authorize_report, only: [:index, :new, :create]
+  skip_before_action :authenticate_user!, only: [:index]
 
   # GET /articles or /articles.json
   def index
